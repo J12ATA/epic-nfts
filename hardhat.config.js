67fa0f4a500a13ff8a5381b573a953331ffebfde
Jose Figueroa <contact@josefig.com>
2021-11-22 18:43:47 -0800

    added rinkeby key via env variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: '0.8.10',
+  networks: {
+    rinkeby: {
+      url: process.env.DEV_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
